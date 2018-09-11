875e87f1c4eaf872cc949cd736d33da4d6cd9270
Herman Mak <hermanmak621@gmail.com>
2018-09-11 15:57:10 -0700

    Update Problem_Set_2_Perceptron_Algorithm.py

--- Problem_Set_2_Perceptron_Algorithm.py
+++ Problem_Set_2_Perceptron_Algorithm.py
@@ -13,7 +13,7 @@ a = random.uniform(-1,0)
 b = random.uniform(-1,0)
 c = random.uniform(0,1)
 
-#iterate update perceptron algorithm 1,000,000 times when there is an error
+#iterate update perceptron algorithm 1,000,000 times, update only when there is an error
 for i in range (0,1000000):
     row = random.randint(0,len(df)-1)
     #find if there is error, if so, execute update algorithm
